1d9779aa075683ef6ead14646f8ecb3a47bc7475
PGross <paul.riedlingen@gmail.com>
2025-03-21 15:48:30 +0100

    new ui build

--- src/gen/file_next_static_MDeAEZM3lj0SywoaBWGRn_ssgManifest_js.hpp
+++ src/gen/file_next_static_MDeAEZM3lj0SywoaBWGRn_ssgManifest_js.hpp
@@ -2,10 +2,10 @@
 #pragma once
 #include "files.hpp"
 namespace duckdb {
-const File FILE_NEXT_STATIC_XSY0NRNPXMG2_YSUXEGMM_SSGMANIFEST_JS = {
+const File FILE_NEXT_STATIC_MDEAEZM3LJ0SYWOABWGRN_SSGMANIFEST_JS = {
      // Content
      "c2VsZi5fX1NTR19NQU5JRkVTVD1uZXcgU2V0KFtdKTtzZWxmLl9fU1NHX01BTklGRVNUX0NCJiZzZWxmLl9fU1NHX01BTklGRVNUX0NCKCk=",    //
      "application/javascript", //
-     "/_next/static/xsY0NrnpxmG2_YsUxEgMM/_ssgManifest.js/", //
+     "/_next/static/MDeAEZM3lj0SywoaBWGRn/_ssgManifest.js/", //
 };
 }
