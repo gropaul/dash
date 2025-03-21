8dcc39e0e1a11a710dce97fec7a964e9684f8b80
PGross <paul.riedlingen@gmail.com>
2025-03-21 16:38:01 +0100

    new extension-ci-tools commit

---
+++ src/gen/file_next_static_xLB0niFlIvvNtEGaOuUoQ_buildManifest_js.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+#include "files.hpp"
+namespace duckdb {
+const File FILE_NEXT_STATIC_XLB0NIFLIVVNTEGAOUUOQ_BUILDMANIFEST_JS = {
+     // Content
+     "c2VsZi5fX0JVSUxEX01BTklGRVNUPXtfX3Jld3JpdGVzOnthZnRlckZpbGVzOltdLGJlZm9yZUZpbGVzOltdLGZhbGxiYWNrOltdfSwiL19lcnJvciI6WyJzdGF0aWMvY2h1bmtzL3BhZ2VzL19lcnJvci1mMjUxYTJlMWMxYjNjNzFjLmpzIl0sc29ydGVkUGFnZXM6WyIvX2FwcCIsIi9fZXJyb3IiXX0sc2VsZi5fX0JVSUxEX01BTklGRVNUX0NCJiZzZWxmLl9fQlVJTERfTUFOSUZFU1RfQ0IoKTs=",    //
+     300, //
+     "application/javascript", //
+     "/_next/static/xLB0niFlIvvNtEGaOuUoQ/_buildManifest.js/", //
+     
+};
+}
---
+++ src/gen/file_next_static_xLB0niFlIvvNtEGaOuUoQ_ssgManifest_js.hpp
@@ -0,0 +1,13 @@
+
+#pragma once
+#include "files.hpp"
+namespace duckdb {
+const File FILE_NEXT_STATIC_XLB0NIFLIVVNTEGAOUUOQ_SSGMANIFEST_JS = {
+     // Content
+     "c2VsZi5fX1NTR19NQU5JRkVTVD1uZXcgU2V0KFtdKTtzZWxmLl9fU1NHX01BTklGRVNUX0NCJiZzZWxmLl9fU1NHX01BTklGRVNUX0NCKCk=",    //
+     108, //
+     "application/javascript", //
+     "/_next/static/xLB0niFlIvvNtEGaOuUoQ/_ssgManifest.js/", //
+     
+};
+}
