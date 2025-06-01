1e0d31a0b324d0476a54d251cbb2ffcce8431d59
PGross <paul.riedlingen@gmail.com>
2025-06-01 18:44:21 +0200

    updated to ddb v1.3.0

--- src/gen/file_next_static_K2fDoJHh_PDZQZVrKuAj1_ssgManifest_js.hpp
+++ src/gen/file_next_static_K2fDoJHh_PDZQZVrKuAj1_ssgManifest_js.hpp
@@ -2,12 +2,12 @@
 #pragma once
 #include "files.hpp"
 namespace duckdb {
-const File FILE_NEXT_STATIC_RFPNVVZUUK6JHS2E_NJQU_SSGMANIFEST_JS = {
+const File FILE_NEXT_STATIC_K2FDOJHH_PDZQZVRKUAJ1_SSGMANIFEST_JS = {
      // Content
      "c2VsZi5fX1NTR19NQU5JRkVTVD1uZXcgU2V0KFtdKTtzZWxmLl9fU1NHX01BTklGRVNUX0NCJiZzZWxmLl9fU1NHX01BTklGRVNUX0NCKCk=",    //
      108, //
      "application/javascript", //
-     "/_next/static/RfPNVvZUUK6JhS2E_njqu/_ssgManifest.js/", //
+     "/_next/static/K2fDoJHh-PDZQZVrKuAj1/_ssgManifest.js/", //
      
 };
 }
