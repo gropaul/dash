f64029bcc0e6aa7e7891bf9c82964273e7853085
PGross <paul.riedlingen@gmail.com>
2025-10-01 23:03:07 +0200

    updated dash-ui

--- src/gen/file_next_static_A8EUAG_Y2Und920NnpPhO_buildManifest_js.hpp
+++ src/gen/file_next_static_A8EUAG_Y2Und920NnpPhO_buildManifest_js.hpp
@@ -2,12 +2,12 @@
 #pragma once
 #include "files.hpp"
 namespace duckdb {
-const File FILE_NEXT_STATIC_S_RU49APMWEG5_BFHDRGG_BUILDMANIFEST_JS = {
+const File FILE_NEXT_STATIC_A8EUAG_Y2UND920NNPPHO_BUILDMANIFEST_JS = {
      // Content
      "c2VsZi5fX0JVSUxEX01BTklGRVNUPWZ1bmN0aW9uKGUscix0KXtyZXR1cm57X19yZXdyaXRlczp7YWZ0ZXJGaWxlczpbXSxiZWZvcmVGaWxlczpbXSxmYWxsYmFjazpbXX0sX19yb3V0ZXJGaWx0ZXJTdGF0aWM6e251bUl0ZW1zOjcsZXJyb3JSYXRlOjFlLTQsbnVtQml0czoxMzUsbnVtSGFzaGVzOjE0LGJpdEFycmF5OlsxLDEsZSwwLGUsZSwwLGUsZSxyLHIscixlLHIscixlLGUscixyLGUsZSxyLHIsZSxlLGUscixlLHIscixlLHIsZSxlLHIscixyLHIsZSxlLGUsZSxlLGUscixlLGUscixlLHIsZSxlLHIscixyLGUsZSxlLGUsZSxyLHIscixyLGUsZSxyLHIscixyLGUsZSxyLHIsZSxlLHIsZSxyLGUscixlLGUscixlLGUsZSxlLGUsZSxyLHIscixlLGUscixlLHIsZSxlLHIscixlLHIsZSxlLHIsZSxlLGUscixyLHIscixlLGUsZSxlLGUsZSxlLHIscixyLHIscixlLHIsZSxlLHIsZSxyLGUscl19LF9fcm91dGVyRmlsdGVyRHluYW1pYzp7bnVtSXRlbXM6cixlcnJvclJhdGU6MWUtNCxudW1CaXRzOnIsbnVtSGFzaGVzOk5hTixiaXRBcnJheTpbXX0sIi9fZXJyb3IiOlsic3RhdGljL2NodW5rcy9wYWdlcy9fZXJyb3ItNzU0YThmOGUwMjMzZWEzOC5qcyJdLHNvcnRlZFBhZ2VzOlsiL19hcHAiLCIvX2Vycm9yIl19fSgxLDAsMWUtNCksc2VsZi5fX0JVSUxEX01BTklGRVNUX0NCJiZzZWxmLl9fQlVJTERfTUFOSUZFU1RfQ0IoKTs=",    //
      932, //
      "application/javascript", //
-     "/_next/static/S-RU49ApMWeG5_BFhdRgG/_buildManifest.js/", //
+     "/_next/static/A8EUAG_Y2Und920NnpPhO/_buildManifest.js/", //
      
 };
 }
--- src/gen/file_next_static_A8EUAG_Y2Und920NnpPhO_ssgManifest_js.hpp
+++ src/gen/file_next_static_A8EUAG_Y2Und920NnpPhO_ssgManifest_js.hpp
@@ -2,12 +2,12 @@
 #pragma once
 #include "files.hpp"
 namespace duckdb {
-const File FILE_NEXT_STATIC_S_RU49APMWEG5_BFHDRGG_SSGMANIFEST_JS = {
+const File FILE_NEXT_STATIC_A8EUAG_Y2UND920NNPPHO_SSGMANIFEST_JS = {
      // Content
      "c2VsZi5fX1NTR19NQU5JRkVTVD1uZXcgU2V0KFtdKTtzZWxmLl9fU1NHX01BTklGRVNUX0NCJiZzZWxmLl9fU1NHX01BTklGRVNUX0NCKCk=",    //
      108, //
      "application/javascript", //
-     "/_next/static/S-RU49ApMWeG5_BFhdRgG/_ssgManifest.js/", //
+     "/_next/static/A8EUAG_Y2Und920NnpPhO/_ssgManifest.js/", //
      
 };
 }
