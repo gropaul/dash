546d6cc9c412837eb585c9e4d6ce23f2b55e4f19
PGross <paul.riedlingen@gmail.com>
2025-10-16 09:33:41 +0200

    updated dash again and again

--- src/gen/file_next_static_MBo1aAX1_T01D11kt_FJe_ssgManifest_js.hpp
+++ src/gen/file_next_static_MBo1aAX1_T01D11kt_FJe_ssgManifest_js.hpp
@@ -2,12 +2,12 @@
 #pragma once
 #include "files.hpp"
 namespace duckdb {
-const File FILE_NEXT_STATIC_OYCAXIKID6HNDXN3DHYKL_SSGMANIFEST_JS = {
+const File FILE_NEXT_STATIC_MBO1AAX1_T01D11KT_FJE_SSGMANIFEST_JS = {
      // Content
      "c2VsZi5fX1NTR19NQU5JRkVTVD1uZXcgU2V0KFtdKTtzZWxmLl9fU1NHX01BTklGRVNUX0NCJiZzZWxmLl9fU1NHX01BTklGRVNUX0NCKCk=",    //
      108, //
      "application/javascript", //
-     "/_next/static/oYCaxikiD6hnDXn3dHYkl/_ssgManifest.js/", //
+     "/_next/static/MBo1aAX1_T01D11kt_FJe/_ssgManifest.js/", //
      
 };
 }
