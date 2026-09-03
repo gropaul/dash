1ef4c622a9443aca20fd985d4abc4a6dd6712057
PGross <paul.riedlingen@gmail.com>
2026-09-04 00:50:15 +0200

    chore(release): v0.4.1 with dash-ui 0.4.1

--- src/gen/file_test__next_test_txt.hpp
+++ src/gen/file_test__next_test_txt.hpp
@@ -4,7 +4,7 @@
 namespace duckdb {
 const File FILE_TEST__NEXT_TEST_TXT = {
      // Content
-     "MToiJFNyZWFjdC5mcmFnbWVudCIKMjpJWzU3MTIxLFtdLCIiXQozOklbNzQ1ODEsW10sIiJdCjQ6W10KMDp7InJzYyI6WyIkIiwiJDEiLCJjIix7ImNoaWxkcmVuIjpbbnVsbCxbIiQiLCIkTDIiLG51bGwseyJwYXJhbGxlbFJvdXRlcktleSI6ImNoaWxkcmVuIiwidGVtcGxhdGUiOlsiJCIsIiRMMyIsbnVsbCx7fV19XV19XSwiaXNQYXJ0aWFsIjpmYWxzZSwic3RhbGVUaW1lIjozMDAsInZhcnlQYXJhbXMiOiIkVzQiLCJidWlsZElkIjoiSWRxLXVjV19LMjl1U0J1MEJ3bDY1In0K",    //
+     "MToiJFNyZWFjdC5mcmFnbWVudCIKMjpJWzU3MTIxLFtdLCIiXQozOklbNzQ1ODEsW10sIiJdCjQ6W10KMDp7InJzYyI6WyIkIiwiJDEiLCJjIix7ImNoaWxkcmVuIjpbbnVsbCxbIiQiLCIkTDIiLG51bGwseyJwYXJhbGxlbFJvdXRlcktleSI6ImNoaWxkcmVuIiwidGVtcGxhdGUiOlsiJCIsIiRMMyIsbnVsbCx7fV19XV19XSwiaXNQYXJ0aWFsIjpmYWxzZSwic3RhbGVUaW1lIjozMDAsInZhcnlQYXJhbXMiOiIkVzQiLCJidWlsZElkIjoiMUVnUldvWmxSQkRtRHdTMEFLMEVJIn0K",    //
      364, //
      "text/plain", //
      "/test/__next.test.txt/", //
